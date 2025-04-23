a85abf2b2c1703ec12eb3c827aa91c5383c48e3a
Ashwani Kumar <works.ashwanikumar@gmail.com>
2025-04-24 00:57:40 +0530

    Create User.java

---
+++ model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() { return username; }
+    public boolean isAdmin() { return isAdmin; }
+}
